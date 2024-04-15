3855270b4fc3348c7bb1b8b98f4b7b76f8422e87
Silas <silasfas@hotmail.com>
2024-04-15 16:22:06 -0300

    criando tarefas com grunt

--- Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
+++ Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
@@ -2,6 +2,15 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
     })
-    
+
+    grunt.registerTask('olaGrunt', function () {
+        const done = this.async()
+        setTimeout(() => {
+            console.log('Ola Grunt')
+            done()
+        }, 3000);
+    })
+
+    grunt.registerTask('default', ['olaGrunt'])
 }
 
