863001065ac950db97dd879bd1f40a7f86460ddd
Silas <silasfas@hotmail.com>
2024-04-15 17:02:53 -0300

    Use Grunt para compilar LESS e SASS

--- Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
+++ Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
@@ -1,9 +1,37 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+
         pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                files: {
+                    'man.css': 'main.less'
+                }
+            },
+            pruduction: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'man.min.css': 'main.less'
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'main2.css': 'main.scss'
+                }
+            }
+        }
     })
 
-    grunt.registerTask('olaGrunt', function () {
+    grunt.registerTask('Ola Grunt', function () {
         const done = this.async()
         setTimeout(() => {
             console.log('Ola Grunt')
@@ -11,6 +39,9 @@ module.exports = function (grunt) {
         }, 3000);
     })
 
-    grunt.registerTask('default', ['olaGrunt'])
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-sass')
+
+    grunt.registerTask('default', ['less', 'sass'])
 }
 
