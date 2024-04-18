9ff9237e17bd30f392ea05574b5b43a6aa5c33f7
Silas <silasfas@hotmail.com>
2024-04-17 23:44:02 -0300

    Comprima HTML com o Grunt

--- Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
+++ Modulo 018 - Introdução ao Grunt/Grunt/gruntfile.js
@@ -1,3 +1,5 @@
+const { option } = require("grunt")
+
 module.exports = function (grunt) {
     grunt.initConfig({
 
@@ -6,15 +8,15 @@ module.exports = function (grunt) {
         less: {
             development: {
                 files: {
-                    'dev/styles/man.css': 'src/styles/main.less'
+                    'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
-            pruduction: {
+            production: { // Corrigindo o nome da configuração
                 options: {
                     compress: true,
                 },
                 files: {
-                    'dist/styles/man.min.css': 'src/styles/main.less'
+                    'dist/styles/main.min.css': 'src/styles/main.less' // Corrigindo o nome do arquivo de saída
                 }
             }
         },
@@ -22,16 +24,74 @@ module.exports = function (grunt) {
             less: {
                 files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                taks: ['replace:dev']
             }
-        }
+        },
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.html': 'src/index.html' // Corrigindo o caminho de saída
+                }
+            }
+        },
+        clean: ['prebuild']
     })
 
     grunt.loadNpmTasks('grunt-contrib-less')
     grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-replace')
+    grunt.loadNpmTasks('grunt-contrib-htmlmin')
+    grunt.loadNpmTasks('grunt-contrib-clean')
 
     grunt.registerTask('default', ['watch'])
-    grunt.registerTask('build', ['less:pruduction'])
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']) // Corrigindo a chamada para 'less:production'
 }
+
 
 
 // module.exports = function (grunt) {
